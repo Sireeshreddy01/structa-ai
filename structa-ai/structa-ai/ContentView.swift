67edaf344f5b05790a3f90fec8754d9da39f4505
Your Name <suhaibking310@gmail.com>
2026-01-23 16:32:40 +0530

    feat: initialize Xcode project structure with essential files and configurations

---
+++ structa-ai/structa-ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  structa-ai
+//
+//  Created by Suhaib on 23/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ structa-ai/structa-ai/structa_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  structa_aiApp.swift
+//  structa-ai
+//
+//  Created by Suhaib on 23/01/26.
+//
+
+import SwiftUI
+
+@main
+struct structa_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
